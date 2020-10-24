7c7ca1c3e0beb2841f93ac0269496b4ddfb8fe8a
Beatriz Rezener <beatrizrezener@gmail.com>
2020-10-24 12:39:08 -0300

    Route configuration

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::resource('category', 'CategoryController');
+Route::resource('expense', 'ExpenseController');
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
